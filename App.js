0b61ac3d451a5ca066e5996bd857f4f68a2742ca
Rabat martin <martin.rabat@gmail.com>
2022-07-08 11:58:58 +0200

    change button

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import Cam from './component/cam';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Cam />
+    </SafeAreaView>
   );
 }
 
